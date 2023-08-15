23cfe2b3b1540230b02541832983f0dbb0172f7e
Jeongho Park <d0lbangi13@naver.com>
2023-08-15 17:59:33 +0900

    FEAT: useQuery Variables

--- movieql-client/src/routes/Movies.js
+++ movieql-client/src/routes/Movies.js
@@ -1,4 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
+import { Link } from "react-router-dom";
 
 const ALL_MOVIES = gql`
   query getMovies {
@@ -22,18 +23,14 @@ export default function Movies() {
     return <h1>Loading...</h1>;
   }
   if (error) {
-    return <h1>Could not fetch :(</h1>;
+    return <h1>Could not fetch...</h1>;
   }
   return (
     <ul>
       <h1>Movies</h1>
       {data.allMovies.map((movie) => (
-        <li key={movie.id}>{movie.title}</li>
-      ))}
-      <h1>Tweets</h1>
-      {data.allTweets.map((tweet) => (
-        <li key={tweet.id}>
-          {tweet.text}/by: {tweet.author.fullName}
+        <li key={movie.id}>
+          <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
         </li>
       ))}
     </ul>
